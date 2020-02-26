bf70f146f272d6c16885db86d7b016fd7918fe60
BenyaMoulain <benjaminiquique@gmail.com>
2020-02-26 20:27:50 -0300

    Cambio de estilo a los botones 2

--- src/components/ficha-personal.ts
+++ src/components/ficha-personal.ts
@@ -69,7 +69,7 @@ export class FichaPersonal extends connect(store)(LitElement) {
         background: red;
       }
       paper-button.custom:hover {
-        background-color: var(--paper-blue-100);
+        background-color: var(--paper-blue-300);
       }
       paper-button.blue {
         background-color: var(--paper-blue-900);
@@ -262,7 +262,7 @@ export class FichaPersonal extends connect(store)(LitElement) {
         opacity: 0.5;
       }
       paper-button.custom:hover {
-        background-color: var(--paper-blue-100);
+        background-color: var(--paper-blue-300);
       }
       paper-button.blue {
         background-color: var(--paper-blue-900);
